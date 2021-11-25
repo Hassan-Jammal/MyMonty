e38fc464a2e34d56cb140389ad985737d3424569
Hassan Jammal <11530122@students.liu.edu.lb>
2021-11-25 15:20:29 +0200

    Added Toggle dark theme

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -1,21 +1,35 @@
 
 export const run = () => {
-    $('.open-nav-btn').on('click', function() {
-        $('.navbar').removeClass('d-none');
-        $('.close-nav-btn').removeClass('d-none');
-        $('.open-nav-btn').addClass('d-none');
-    });
-    
-    $('.close-nav-btn').on('click', function() {
-        $('.navbar').addClass('d-none');
-        $('.close-nav-btn').addClass('d-none');
-        $('.open-nav-btn').removeClass('d-none');
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+    const links = document.querySelectorAll(".nav-links li");
+
+    hamburger.addEventListener('click', () => {
+        //Animate Links
+        navLinks.classList.toggle("open");
+        links.forEach(link => {
+            link.classList.toggle("fading");
+        });
+
+        //Hamburger Animation
+        hamburger.classList.toggle("toggle");
     });
 
-    gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
-    ScrollTrigger.create({
-        start: 'top -40',
-        end: 99999,
-        toggleClass: { className: 'header--scrolled', targets: '.header' }
-    });
+    var toggle = document.getElementById("language");
+    var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark-mode)").matches ? "dark-mode" : "light-mode");
+    if (storedTheme)
+        document.documentElement.setAttribute('data-theme', storedTheme)
+
+    toggle.onclick = function() {
+        var currentTheme = document.documentElement.getAttribute("data-theme");
+        var targetTheme = "light-mode";
+
+        if (currentTheme === "light-mode") {
+            targetTheme = "dark-mode";
+        }
+
+        document.documentElement.setAttribute('data-theme', targetTheme)
+        localStorage.setItem('theme', targetTheme);
+    };
+
 }
