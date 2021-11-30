6969972d7290e7ef8a72b2dfd21ac6c5c5874b47
Hassan Jammal <11530122@students.liu.edu.lb>
2021-11-30 16:57:21 +0200

    fixes

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -89,6 +89,30 @@ export const run = () => {
         
     animateText();
 
-    alert(navigator.appVersion);
+    // alert(navigator.appVersion);
+
+        var userAgent = window.navigator.userAgent,
+            platform = window.navigator.platform,
+            macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+            windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+            iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+            os = null;
+      
+        if (macosPlatforms.indexOf(platform) !== -1) {
+            document.getElementById("downloadStore").src = "./assets/apple.png";
+        } else if (iosPlatforms.indexOf(platform) !== -1) {
+            document.getElementById("downloadStore").src = "./assets/apple.png";
+        } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        } else if (/Android/.test(userAgent)) {
+            document.getElementById("downloadStore").src = "./assets/android.png";
+        } else if (!os && /Linux/.test(platform)) {
+            os = 'Linux';
+        }
+
+
+      
+        // return os;
+      
+    //   alert(getOS());
 
 }
