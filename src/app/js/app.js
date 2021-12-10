38fde7e14014be567015258bc340faa3329ee984
Hassan Jammal <hassan-jammal@hotmail.com>
2021-12-10 10:59:50 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -6,25 +6,25 @@
 
 var toggle = document.getElementsByClassName("theme-mode");
 var storedTheme =
-  localStorage.getItem("theme") ||
-  (window.matchMedia("(prefers-color-scheme: dark-mode)").matches
-    ? "dark-mode"
-    : "light-mode");
+localStorage.getItem("theme") ||
+(window.matchMedia("(prefers-color-scheme: dark-mode)").matches
+	? "dark-mode"
+	: "light-mode");
 if (storedTheme)
-  document.documentElement.setAttribute("data-theme", storedTheme);
+document.documentElement.setAttribute("data-theme", storedTheme);
 
 for (var i = 0; i < toggle.length; i++) {
-  toggle[i].onclick = function () {
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-    var targetTheme = "light-mode";
+toggle[i].onclick = function () {
+	var currentTheme = document.documentElement.getAttribute("data-theme");
+	var targetTheme = "light-mode";
 
-    if (currentTheme === "light-mode") {
-      targetTheme = "dark-mode";
-    }
+	if (currentTheme === "light-mode") {
+	targetTheme = "dark-mode";
+	}
 
-    document.documentElement.setAttribute("data-theme", targetTheme);
-    localStorage.setItem("theme", targetTheme);
-  };
+	document.documentElement.setAttribute("data-theme", targetTheme);
+	localStorage.setItem("theme", targetTheme);
+};
 }
 
 /*******************************
@@ -43,21 +43,21 @@ var monthly = document.getElementsByClassName("monthly");
 var yearly = document.getElementsByClassName("yearly");
 
 function checkMembership(event) {
-  if (event.target.id == "monthly") {
-    for (var i = 0; i < monthly.length; i++) {
-      monthly[i].style.display = "inline-block";
-      yearly[i].style.display = "none";
-    }
-  } else {
-    for (var i = 0; i < yearly.length; i++) {
-      yearly[i].style.display = "inline-block";
-      monthly[i].style.display = "none";
-    }
-  }
+if (event.target.id == "monthly") {
+	for (var i = 0; i < monthly.length; i++) {
+	monthly[i].style.display = "inline-block";
+	yearly[i].style.display = "none";
+	}
+} else {
+	for (var i = 0; i < yearly.length; i++) {
+	yearly[i].style.display = "inline-block";
+	monthly[i].style.display = "none";
+	}
+}
 }
 
 document.querySelectorAll("input[name='membership']").forEach((input) => {
-  input.addEventListener("change", checkMembership);
+input.addEventListener("change", checkMembership);
 });
 
 /*******************************
@@ -68,65 +68,23 @@ document.querySelectorAll("input[name='membership']").forEach((input) => {
 
 /*******************************
  *
- * START Hero text animation
- *
- *******************************/
-
-const txts = document.querySelector(".animate-text").children,
-  txtsLen = txts.length;
-const textInTimer = 2500,
-  textOutTimer = 2200;
-let index = 0;
-
-function animateText() {
-  for (let i = 0; i < txtsLen; i++) {
-    txts[i].classList.remove("text-in", "text-out");
-  }
-  txts[index].classList.add("text-in");
-
-  setTimeout(function () {
-    txts[index].classList.add("text-out");
-  }, textOutTimer);
-
-  setTimeout(function () {
-    if (index == txtsLen - 1) {
-      index = 0;
-    } else {
-      index++;
-    }
-    animateText();
-  }, textInTimer);
-}
-
-animateText();
-
-/*******************************
- *
- * END Hero text animation
- *
- *******************************/
-
-/*******************************
- *
  * START Toggle Mobile Menu Animation
  *
  *******************************/
 
-let subMenu = document.querySelector(".sub-menu");
-let navLinks = document.querySelector(".nav-links");
 let menuBtn = document.querySelector(".menu");
-let hasSubMenu = document.querySelector(".has-sub-menu");
-let arrow = document.querySelector(".has-sub-menu span svg");
+let navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  menuBtn.classList.toggle("toggle");
+navLinks.classList.toggle("open");
+menuBtn.classList.toggle("toggle");
 });
 
-hasSubMenu.onclick = function () {
-  subMenu.classList.toggle("d-block");
-  arrow.classList.toggle("rotate");
-};
+$('.menu-item-has-children').on("click", function(){
+$(this).next('.sub-menu-wrapper').toggle();
+$(this).find('svg').toggleClass('rotate');
+});
+
 
 /*******************************
  *
@@ -141,8 +99,8 @@ hasSubMenu.onclick = function () {
  *******************************/
 
 window.onscroll = function () {
-  let image = document.getElementById("website-logo");
-  image.style.transform = "rotate(" + window.pageYOffset / 5 + "deg)";
+let image = document.getElementById("website-logo");
+image.style.transform = "rotate(" + window.pageYOffset / 5 + "deg)";
 };
 
 /*******************************
@@ -155,19 +113,36 @@ window.onscroll = function () {
  *
  * START Selecting Language
  *
- * MUST READ
- * This function let you choose between languages.
- * when you choose a language, you should add "selected" attribute to the img in the languages section
- * and then let the magic do its work
- *
  *******************************/
 
-let languagesWrapper = document.querySelector(".languages");
-let languageToggler = document.querySelector(".language-toggler");
-let language = document.querySelectorAll(".languages-wrapper__list-language");
+// (Desktop)
+let desktopLanguagesWrapper = document.querySelector(
+".desktop-header .languages"
+);
+let desktopLanguageToggler = document.querySelector(
+".desktop-header .language-toggler"
+);
+let desktopLanguage = document.querySelector(
+".desktop-header .languages-wrapper__list-language"
+);
 
-languageToggler.onclick = function () {
-  languagesWrapper.classList.toggle("open");
+desktopLanguageToggler.onclick = function () {
+desktopLanguagesWrapper.classList.toggle("open");
+};
+
+// (Mobile)
+let mobileLanguagesWrapper = document.querySelector(
+".mobile-header .languages"
+);
+let mobileLanguageToggler = document.querySelector(
+".mobile-header .language-toggler"
+);
+let mobileLanguage = document.querySelector(
+".mobile-header .languages-wrapper__list-language"
+);
+
+mobileLanguageToggler.onclick = function () {
+mobileLanguagesWrapper.classList.toggle("open");
 };
 
 /*******************************
@@ -182,16 +157,40 @@ languageToggler.onclick = function () {
  *
  *******************************/
 
-let locationsWrapper = document.querySelector(".locations");
-let toggleLocations = document.querySelector(".toggle-locations");
-let location = document.querySelectorAll(".locations-wrapper__list-location");
-let logo = document.querySelector(".logo");
-toggleLocations.onclick = function () {
-  locationsWrapper.classList.toggle("open");
-  for (var i = 0; i < location.length; i++) {
-    location[i].classList.toggle("toggle");
-  }
-  logo.classList.toggle("open");
+// (Desktop)
+let desktopLocationsWrapper = document.querySelector(
+".desktop-header .locations"
+);
+let desktopLocationsToggler = document.querySelector(
+".desktop-header .toggle-locations"
+);
+let desktopLocation = document.querySelector(
+".desktop-header .locations-wrapper__list-location"
+);
+let desktopLogo = document.querySelector(".desktop-header .logo");
+
+desktopLocationsToggler.onclick = function () {
+desktopLocationsWrapper.classList.toggle("open");
+desktopLocation.classList.toggle("toggle");
+desktopLogo.classList.toggle("open");
+};
+
+// (Mobile)
+let mobileLocationsWrapper = document.querySelector(
+".mobile-header .locations"
+);
+let mobileLocationsToggler = document.querySelector(
+".mobile-header .toggle-locations"
+);
+let mobileLocation = document.querySelector(
+".mobile-header .locations-wrapper__list-location"
+);
+let mobileLogo = document.querySelector(".mobile-header .logo");
+
+mobileLocationsToggler.onclick = function () {
+mobileLocationsWrapper.classList.toggle("open");
+mobileLocation.classList.toggle("toggle");
+mobileLogo.classList.toggle("open");
 };
 
 /*******************************
@@ -199,3 +198,123 @@ toggleLocations.onclick = function () {
  * END Logos Dropdown
  *
  *******************************/
+
+window.onload = () => {
+const menuDimensions = {
+	plans: { height: 0, width: 0 },
+	features: { height: 0, width: 0 },
+	features: { height: 0, width: 0 },
+	plans: { height: 0, width: 0 },
+};
+
+const menuOptions = document.querySelectorAll("[data-menu-opt]");
+const menuSections = document.querySelectorAll("[data-section]");
+const menuContainer = document.querySelector(".desktop-menu");
+const menuPointer = document.querySelector(".menu-pointer");
+const headerContainer = document.querySelector(".header");
+const optionsBar = document.querySelector(".desktop-nav__tabs");
+
+menuContainer.classList.add("desktop-menu--rotate");
+
+for (section of menuSections) {
+	const sectionName = section.dataset.section;
+	menuDimensions[sectionName] = {
+	height: Math.ceil(section.getBoundingClientRect().height),
+	width: Math.ceil(section.getBoundingClientRect().width),
+	};
+}
+
+for (element of menuOptions) {
+	const targetSection = element.dataset.menuOpt;
+	const containerLeftEdge = optionsBar.getBoundingClientRect().x;
+	const sourceElRect = element.getBoundingClientRect();
+	const sourceCenter = Math.round(
+	sourceElRect.x + sourceElRect.width / 2
+	);
+
+	element.addEventListener("mouseenter", (e) => {
+	const targetElement = document.querySelector(
+		`.section-${targetSection}`
+	);
+
+	for (section of menuSections) {
+		section.classList.remove("desktop-menu__section--visible");
+	}
+
+	menuContainer.setAttribute(
+		"style",
+		`height: ${menuDimensions[targetSection].height}px; width: ${menuDimensions[targetSection].width}px;`
+	);
+
+	if (menuPointer.classList.contains("menu-pointer--visible")) {
+		menuPointer.setAttribute(
+		"style",
+		`transform: translateY(-6px) translateX(${Math.floor(
+			sourceCenter - containerLeftEdge - 8
+		)}px) rotate(45deg)`
+		);
+	} else {
+		menuPointer.setAttribute(
+		"style",
+		`transform: translateX(${Math.floor(
+			sourceCenter - containerLeftEdge - 8
+		)}px) rotate(45deg)`
+		);
+	}
+
+	menuContainer.classList.add("desktop-menu--visible");
+	targetElement.classList.add("desktop-menu__section--visible");
+	menuContainer.addEventListener("transitionend", addResizeAnim);
+	menuContainer.addEventListener("transitionend", positionArrow);
+	});
+}
+
+const clearAfterLeaving = () => {
+	menuContainer.classList.remove("desktop-menu--anim-resize");
+	menuPointer.classList.remove("menu-pointer--anim-transform");
+	menuPointer.classList.remove("menu-pointer--visible");
+	menuPointer.removeAttribute("style");
+	for (section of menuSections) {
+	section.removeAttribute("style");
+	}
+	menuContainer.removeAttribute("style");
+	menuContainer.removeEventListener(
+	"transitionstart",
+	clearAfterLeaving
+	);
+};
+
+const positionArrow = () => {
+	if (!menuPointer.classList.contains("menu-pointer--visible")) {
+	const styles = menuPointer.getAttribute("style");
+	if (
+		styles &&
+		menuContainer.classList.contains("desktop-menu--visible")
+	) {
+		const newStyles = styles.replace("transform: ", "");
+		menuPointer.setAttribute(
+		"style",
+		`transform: translateY(-6px) ${newStyles}`
+		);
+		menuPointer.classList.add("menu-pointer--visible");
+		menuPointer.classList.add("menu-pointer--anim-transform");
+	}
+	}
+	menuContainer.removeEventListener("transitionend", positionArrow);
+};
+
+const addResizeAnim = () => {
+	if (!menuContainer.classList.contains("desktop-menu--anim-resize")) {
+	menuContainer.classList.add("desktop-menu--anim-resize");
+	for (section of menuSections) {
+		section.setAttribute("style", "transition-duration: 0.25s");
+	}
+	}
+	menuContainer.removeEventListener("transitionend", addResizeAnim);
+};
+
+headerContainer.addEventListener("mouseleave", () => {
+	menuContainer.addEventListener("transitionstart", clearAfterLeaving);
+	menuContainer.classList.remove("desktop-menu--visible");
+});
+};
